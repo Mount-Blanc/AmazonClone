86cf92ab222cf71375d444e368031c75e2740ce4
Mount-Blanc <mountblanc100@gmail.com>
2023-05-08 12:34:56 -0500

    added options to select

--- AmazonClone/src/App.jsx
+++ AmazonClone/src/App.jsx
@@ -25,7 +25,9 @@ function App() {
             <div className='selector'>
               <span>All</span>
             </div>
-            <select class="select" title='Search'></select>
+            <select class="select" title='Search'>
+            <option selected="selected" value="search-alias=aps">All Departments</option>
+            </select>
             <div className='nav-fill'>
          <input className='nav-input' type="search" placeholder='Search Amazon'></input>
 
@@ -62,8 +64,22 @@ function App() {
       </div>
       </div>
       <div className='nav-main'>
-        <div className='nav-left'>All</div>
-        <div className='nav-fill'></div>
+        <div >All</div>
+        <div className='nav-fill'>
+          <a href='' className='nav-a'>Customer Service</a>
+          <a href=''className='nav-a'>Best Sellers</a>
+          <a href className='nav-a'>New Releases</a>
+          <a href='' className='nav-a'>Prime</a>
+          <a href='' className='nav-a'>Today's Deals</a>
+          <a href='' className='nav-a'>Music</a>
+          <a href='' className='nav-a'>Books</a>
+          <a href='' className='nav-a'>Registry</a>
+          <a href='' className='nav-a'>Fashion</a>
+          <a href=''className='nav-a'>Amazon Home</a>
+          <a href=''className='nav-a'>Gift Cards</a>
+          <a href='' className='nav-a'>Pharmacy</a>
+
+        </div>
       </div>
      </header>
      <nav>nav</nav>
