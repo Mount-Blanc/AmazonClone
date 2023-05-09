b26ed453821dda9ea2a7730b65de21c11a0ad38b
Mount-Blanc <mountblanc100@gmail.com>
2023-05-09 11:27:30 -0500

    added card css

--- AmazonClone/src/Card.jsx
+++ AmazonClone/src/Card.jsx
@@ -1,10 +1,14 @@
+
+
 function Card () {
     return(
         <>
-     <div>
-        <div>HEader</div>
+     <div className="card-1">
+        <div><h2>Deal 1</h2></div>
         <div>body</div>
-        <div>footer</div>
+        <div>
+            <a href=""><span>Corresponding Link</span></a>
+        </div>
      </div>
         </>
     )
