28b33924800f6488cbd6b9a50c1abc1d4993f0f6
Mount-Blanc <mountblanc100@gmail.com>
2023-05-09 11:35:46 -0500

    added styling to deal cards

--- AmazonClone/src/Card.jsx
+++ AmazonClone/src/Card.jsx
@@ -1,8 +1,8 @@
 
-
+import './Card.css'
 function Card () {
     return(
-        <>
+        <div className="section-1">
      <div className="card-1">
         <div><h2>Deal 1</h2></div>
         <div>body</div>
@@ -10,7 +10,26 @@ function Card () {
             <a href=""><span>Corresponding Link</span></a>
         </div>
      </div>
-        </>
+     <div className="card-1">
+        <div><h2>Deal 1</h2></div>
+        <div>body</div>
+        <div>
+            <a href=""><span>Corresponding Link</span></a>
+        </div>
+     </div><div className="card-1">
+        <div><h2>Deal 1</h2></div>
+        <div>body</div>
+        <div>
+            <a href=""><span>Corresponding Link</span></a>
+        </div>
+     </div><div className="card-1">
+        <div><h2>Deal 1</h2></div>
+        <div>body</div>
+        <div>
+            <a href=""><span>Corresponding Link</span></a>
+        </div>
+     </div>
+        </div>
     )
 }
 export default Card
